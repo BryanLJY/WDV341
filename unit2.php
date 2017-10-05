75ba30fb0a646bb4fc72569eef88c50f1adf4335
bryanljy <32549964+bryanljy@users.noreply.github.com>
2017-10-05 14:34:18 -0500

    Add files via upload

---
+++ unit2.php
@@ -0,0 +1,29 @@
+<?php
+
+$yourName = "Bryan Lee";
+
+$number1 = "6";
+
+$number2 = "22";
+
+$number3 = $number1 + $number2;
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Assignment: PHP Basics</title>
+</head>
+
+<body>
+<h1>Assignment Unit 2: PHP Basics</h1>
+<h2>My name is: <?php echo "$yourName"; ?></h2>
+<p>The value of the first number is: <?php echo "$number1"; ?></p>
+<p>The value of the second number is: <?php echo "$number2"; ?></p>
+<p>The sum of the first and second number is: <?php echo "$number3"; ?></p>
+</body>
+
+</html>
